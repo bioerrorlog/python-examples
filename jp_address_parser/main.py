86a3efc9219cd51e34a48665195b53bd98a5b291
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-12-05 08:10:58 +0900

    flow designed

--- jp_address_parser/main.py
+++ jp_address_parser/main.py
@@ -1,34 +1,28 @@
 import os
-import time
-import openai
-
-openai.api_key = os.environ["OPENAI_API_KEY"]
+from openai import OpenAI
 
 
-def main() -> None:
-    start_time = time.time()
+def parse_address(address: str, client: OpenAI) -> dict:
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0613",
+    response = client.ChatCompletion.create(
+        model="gpt-3.5-turbo-1106",
         messages=[
             {'role': 'user', 'content': 'Tell me about the Japanese history.'}
         ],
-        stream=True,
-        request_timeout=3,
     )
 
-    collected_chunks = []
-    collected_messages = []
-    for chunk in response:
-        chunk_time = time.time() - start_time
+    return response
 
-        collected_chunks.append(chunk)
-        chunk_message = chunk['choices'][0]['delta'].get('content', '')
-        collected_messages.append(chunk_message)
-        print(f"Message received {chunk_time:.2f} seconds after request: {chunk_message}")
 
-    full_reply_content = ''.join(collected_messages)
-    print(f"Full conversation received: {full_reply_content}")
+def main() -> None:
+    client = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+
+    address = '港区赤坂一丁目2の3'
+
+    result = parse_address(address, client)
+    print(result)
 
 
 if __name__ == "__main__":
