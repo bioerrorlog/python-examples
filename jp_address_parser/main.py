b669c36bb8810cfac0010863f0f43ce680ee3b8e
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-12-05 08:29:55 +0900

    reference worked

--- jp_address_parser/main.py
+++ jp_address_parser/main.py
@@ -3,15 +3,35 @@ from openai import OpenAI
 
 
 def parse_address(address: str, client: OpenAI) -> dict:
+    system_prompt = """日本の住所を以下の項目に沿ってJSON形式で正確にパースし、JSONのみを出力すること:
 
-    response = client.ChatCompletion.create(
+# 項目
+- 都道府県
+- 郡名
+- 市区町村
+- 町域
+- 丁目
+- 番地
+- 号
+- 建物名・部屋番号
+
+# 規則
+- 都道府県が省略されている場合は適切に補うこと
+- ハイフンを残さないこと
+- 固有名詞に含まれない漢数字は半角数字に置換すること
+- ない場合は空欄にすること
+"""
+
+    response = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
+        response_format={"type": "json_object"},
         messages=[
-            {'role': 'user', 'content': 'Tell me about the Japanese history.'}
+            {'role': 'system', 'content': system_prompt},
+            {'role': 'user', 'content': address},
         ],
     )
 
-    return response
+    return response.choices[0].message.content
 
 
 def main() -> None:
