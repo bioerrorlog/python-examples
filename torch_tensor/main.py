74125161f6d68f8bf9ed197dc00468c06ddda1c8
BioErrorLog <51422347+bioerrorlog@users.noreply.github.com>
2024-03-03 11:38:16 +0000

    tinkering torch.tensor

--- torch_tensor/main.py
+++ torch_tensor/main.py
@@ -1,15 +1,27 @@
 import torch
 
 
-def main() -> None:
-    x = torch.tensor([1, 2, 3])
-    print(x)
-    print(x.dtype)
+x = torch.tensor([1, 2, 3])
+print(x)
+print(x.dtype)
 
-    X = torch.Tensor([1, 2, 3])
-    print(X)
-    print(X.dtype)
+y = torch.tensor([1, 2, 3], dtype=torch.float32)
+print(y)
+print(y.dtype)
 
+X = torch.Tensor([1, 2, 3])
+print(X)
+print(X.dtype)
 
-if __name__ == "__main__":
-    main()
+Empty = torch.Tensor()
+print(Empty)
+print(Empty.dtype)
+
+empty = torch.tensor(())
+print(empty)
+print(empty.dtype)
+
+# Error
+empty_err = torch.tensor()
+print(empty_err)
+print(empty_err.dtype)
