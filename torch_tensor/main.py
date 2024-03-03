e5681949de93e2af058192bcd6067ca77298e18f
bioerrorlog <bioerrorlog.contact@gmail.com>
2024-03-03 17:40:03 +0900

    tensor vs Tensor

---
+++ torch_tensor/main.py
@@ -0,0 +1,14 @@
+import torch
+
+
+def main() -> None:
+    x = torch.tensor([1, 2, 3])
+    print(x)
+    print(x.dtype)
+
+    X = torch.Tensor([1, 2, 3])
+    print(X)
+    print(X.dtype)
+
+if __name__ == "__main__":
+    main()
